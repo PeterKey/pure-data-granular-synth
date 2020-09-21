10e6bab61ed84954f79b19e5dac62e503a6fc475
Peter <peter.gorzo@posteo.net>
2020-09-21 11:43:22 +0200

    First compilable version

--- Grainmaker/grainmaker.c
+++ Grainmaker/grainmaker.c
@@ -7,25 +7,34 @@
 //
 
 #include "m_pd.h"
+#include "stdlib.h"
 
 static t_class *grainmaker_tilde_class;
 
 typedef struct _grainmaker_tilde {
     t_object    x_obj;
-    t_int       offset, grain_length;
-    t_float     head_pos, playback_speed;
-    t_symbol   *x_arrayname;
+    t_word      *x_vec;
+    int         x_npoints;
+    t_symbol    *x_arrayname;
     t_float     t_f;
-    // sample, grain_envelope, (grain_start), direction
-    t_array     *sample;
-    t_inlet     *in_sample, *in_offset, *in_grain_length, *in_head_pos;
+    
+    
+//    t_int       offset, grain_length;
+//    t_float     head_pos, playback_speed;
+//    // sample, grain_envelope, (grain_start), direction
+//    t_array     *sample;
+//    t_inlet     *in_sample, *in_offset, *in_grain_length, *in_head_pos;
 //    t_outlet    *out_A, *out_B, *out_synch, *out_count;
 }t_grainmaker_tilde;
 
-void *grainmaker_tilde_new(t_floatarg f1, t_floatarg f2) {
+void *grainmaker_tilde_new(t_symbol *s) {
     t_grainmaker_tilde *x = (t_grainmaker_tilde *)pd_new(grainmaker_tilde_class);
     
-    x->in_sample = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_list, gensym("sample"));
+    x->x_arrayname = s;
+    x->x_vec = 0;
+    outlet_new(&x->x_obj, gensym("signal"));
+    x->t_f = 0;
+//    x->in_sample = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_list, gensym("sample"));
 
 //    x->out_A = outlet_new(&x->x_obj, &s_bang);
 //    x->out_B = outlet_new(&x->x_obj, &s_bang);
@@ -33,6 +42,52 @@ void *grainmaker_tilde_new(t_floatarg f1, t_floatarg f2) {
 //    x->out_count = outlet_new(&x->x_obj, &s_float);
     
     return (void *)x;
+}
+
+static t_int *grainmaker_tilde_perform(t_int *w)
+{
+    t_grainmaker_tilde *x = (t_grainmaker_tilde *)(w[1]);
+    t_sample *in = (t_sample *)(w[2]);
+    t_sample *out = (t_sample *)(w[3]);
+    int n = (int)(w[4]);
+    int maxindex;
+    t_word *buf = x->x_vec;
+    int i;
+    
+    int length = snprintf( NULL, 0, "%d", n );
+    char* str = malloc( length + 1 );
+    snprintf( str, length + 1, "%d", n );
+    
+    post(str);
+    
+    free(str);
+
+    maxindex = x->x_npoints - 1;
+    if(maxindex<0) goto zero;
+    if (!buf) goto zero;
+
+    for (i = 0; i < n; i++)
+    {
+        int index = *in++;
+        if (index < 0)
+            index = 0;
+        else if (index > maxindex)
+            index = maxindex;
+        *out++ = buf[index].w_float;
+    }
+    return (w+5);
+ zero:
+    while (n--) *out++ = 0;
+
+    return (w+5);
+}
+
+void grainmaker_tilde_free(t_grainmaker_tilde *x) {
+
+//    outlet_free(x->out_A);
+//    outlet_free(x->out_B);
+//    outlet_free(x->out_synch);
+//    outlet_free(x->out_count);
 }
 
 static void grainmaker_tilde_set(t_grainmaker_tilde *x, t_symbol *s)
@@ -64,52 +119,15 @@ static void grainmaker_tilde_dsp(t_grainmaker_tilde *x, t_signal **sp)
 
 }
 
-static t_int *grainmaker_tilde_perform(t_int *w)
-{
-    t_grainmaker_tilde *x = (t_grainmaker_tilde *)(w[1]);
-    t_sample *in = (t_sample *)(w[2]);
-    t_sample *out = (t_sample *)(w[3]);
-    int n = (int)(w[4]);
-    int maxindex;
-    t_word *buf = x->x_vec;
-    int i;
-
-    maxindex = x->x_npoints - 1;
-    if(maxindex<0) goto zero;
-    if (!buf) goto zero;
-
-    for (i = 0; i < n; i++)
-    {
-        int index = *in++;
-        if (index < 0)
-            index = 0;
-        else if (index > maxindex)
-            index = maxindex;
-        *out++ = buf[index].w_float;
-    }
-    return (w+5);
- zero:
-    while (n--) *out++ = 0;
-
-    return (w+5);
-}
-
-void grainmaker_tilde_free(t_grainmaker_tilde *x) {
-    inlet_free(x->in_sample);
-
-//    outlet_free(x->out_A);
-//    outlet_free(x->out_B);
-//    outlet_free(x->out_synch);
-//    outlet_free(x->out_count);
-}
-
-void grainmaker_setup(void) {
-    grainmaker_class = class_new(gensym("grainmaker~"),
-                                 (t_newmethod)grainmaker_new,
-                                 (t_method)grainmaker_free,
-                                 sizeof(t_grainmaker),
-                                 CLASS_DEFAULT,
+void grainmaker_tilde_setup(void) {
+    grainmaker_tilde_class = class_new(gensym("grainmaker~"),
+                                 (t_newmethod)grainmaker_tilde_new,
+                                 (t_method)grainmaker_tilde_free,
+                                 sizeof(t_grainmaker_tilde),
+                                 0,
+                                 A_DEFSYM,
                                  0);
-    
-    class_addbang(grainmaker_class, (t_method)grainmaker_onBangMsg);
+    CLASS_MAINSIGNALIN(grainmaker_tilde_class, t_grainmaker_tilde, t_f);
+    class_addmethod(grainmaker_tilde_class, (t_method)grainmaker_tilde_dsp,
+        gensym("dsp"), A_CANT, 0);
 }
