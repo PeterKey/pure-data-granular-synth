beb72bfb60eb1b8b01a0c448a46917fb94f79429
Peter <peter.gorzo@posteo.net>
2020-09-28 18:59:02 +0200

    created main loop

--- Grainmaker/grain.c
+++ Grainmaker/grain.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
-#include "grain.h"
 #include <m_pd.h>
+#include "grain.h"
+
+int minimumInt(int a, int b) {
+    return a<b ? a : b;
+}
 
 grain grain_new(int start_sample, int end_sample) {
     grain x;
     x.start_sample = start_sample;
     x.end_sample = end_sample;
-    x.current_pos = 0;
+    x.current_sample = start_sample;
+    x.grain_size = end_sample - start_sample;
     
     return x;
 }
@@ -18,8 +23,15 @@ grain construct_grain(int sample_pos, int src_sample_length, int offset, int gra
     int max_range = src_sample_length - grain_length;
     if (range_end > max_range) range_end = max_range;
 
-    int start_sample = range_start + rand() % (range_end - range_start);
-    grain new_grain = grain_new(start_sample, (start_sample + grain_length));
+    int range_size = range_end - range_start;
+    int start_sample;
+    if (range_size > 0) {
+        start_sample = range_start + rand() % (range_end - range_start);
+    } else {
+        start_sample = 0;
+    }
+    int end_sample = minimumInt(src_sample_length, start_sample + grain_length);
+    grain new_grain = grain_new(start_sample, end_sample);
     
     post("new grain constructed");
     return new_grain;
--- Grainmaker/grain.h
+++ Grainmaker/grain.h
@@ -8,7 +8,8 @@ extern "C" {
 typedef struct grain {
     int start_sample,
         end_sample,
-        current_pos;
+        current_sample,
+        grain_size;
 }grain;
 
 grain construct_grain(int sample_pos, int src_sample_length, int offset, int grain_length);
